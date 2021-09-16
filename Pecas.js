3a748f4110849a4885ad57ddbfdbaf23f8f04a2b
Tiaguinhoo <89615412+Tiaguinhoo@users.noreply.github.com>
2021-09-16 16:47:31 -0300

    Update Pecas.js

--- Pecas.js
+++ Pecas.js
@@ -6,25 +6,25 @@ const pesoPeca = 101;
 console.log ('Insira o nome da peça:')
 if (listaPecas.length <=10) {
 listaPecas.push ('SDD')
-for(index = 10; index < listaPecas.length; index++) {
-const nome = listaPecas[index];
-if (nome.length >= 3) {
-console.log (nome + ' -- Cadastrando...')
-console.log ('Insira o peso da peça em (g): ')
-if (pesoPeca >= 100) {
-console.log (pesoPeca,'g')
-console.log ('Cadastro de Peça efetuado com sucesso!')
-console.log (listaPecas)
-} else {
-console.log (pesoPeca,'g')
-console.log ('Peso menor que o permitido (100g)')
-console.log ('Não foi possivel prosseguir com o cadastro.')
-}
-} else {
-console.log (nome + ' -- Nome muito pequeno')
-console.log ('Não foi possivel prosseguir com o cadastro.')
-}
-}
+    for(index = 10; index < listaPecas.length; index++) {
+    const nome = listaPecas[index];
+    if (nome.length >= 3) {
+        console.log (nome + ' -- Cadastrando...')
+        console.log ('Insira o peso da peça em (g): ')
+        if (pesoPeca >= 100) {
+        console.log (pesoPeca,'g')
+        console.log ('Cadastro de Peça efetuado com sucesso!')
+        console.log (listaPecas)
+        } else {
+            console.log (pesoPeca,'g')
+            console.log ('Peso menor que o permitido (100g)')
+            console.log ('Não foi possivel prosseguir com o cadastro.')
+        }
+    } else {
+    console.log (nome + ' -- Nome muito pequeno')
+    console.log ('Não foi possivel prosseguir com o cadastro.')
+    }
+    }
 }
 else {
 console.log ('Quantidade de peças excedida (10)')
